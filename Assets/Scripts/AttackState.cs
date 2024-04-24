ad8221fc20b4fb6d6a597070b6184efd7d1b4fde
Axel Goré <axel.gore@outlook.fr>
2024-04-24 16:57:21 +0200

    Zombies assets, first round of animations pack and pathfinding

---
+++ Assets/Scripts/AttackState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AttackState : StateMachineBehaviour
+{
+	UnityEngine.AI.NavMeshAgent agent;
+	Transform player;
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		agent = animator.GetComponent<UnityEngine.AI.NavMeshAgent>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		animator.transform.LookAt(player);
+		float distance = Vector3.Distance(player.position, animator.transform.position);
+		if (distance >= 3.0f)
+		{
+			animator.SetBool("isAttacking", false);
+			animator.SetBool("isWalking", true);
+			
+		}
+
+	}
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		agent.SetDestination(animator.transform.position);
+
+	}
+}
