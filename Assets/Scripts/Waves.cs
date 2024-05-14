9bb99f0812fe5d6b6aed3d3f18f409db244a3a1c
Nicolas Bouté <nicolasboute123@gmail.com>
2024-05-14 10:22:39 +0200

    added comments on scripts

--- Assets/Scripts/Waves.cs
+++ Assets/Scripts/Waves.cs
@@ -2,47 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This class controls the spawning of waves of zombies
 public class Waves : MonoBehaviour
 {
-    public int waveNumber = 1;
-    public int zombieNumber = 10;
-    public GameObject[] zombiesPrefab;
-    public GameObject[] spawnPoints;
-    public static Waves instance;
-    private AudioSource audioSource;
+	// The current wave number
+	public int waveNumber = 1;
 
-    void Awake()
-    {
+	// The number of zombies to spawn in the current wave
+	public int zombieNumber = 10;
+
+	// An array of zombie prefabs to spawn
+	public GameObject[] zombiesPrefab;
+
+	// An array of spawn points where zombies can appear
+	public GameObject[] spawnPoints;
+
+	// A singleton instance of the Waves class
+	public static Waves instance;
+
+	// The AudioSource component attached to this GameObject
+	private AudioSource audioSource;
+
+	// Awake is called when the script instance is being loaded
+	void Awake()
+	{
+		// If there is no instance of Waves yet, set this as the instance
 		if (instance == null)
 			instance = this;
-        audioSource = GetComponent<AudioSource>();
+
+		// Get the AudioSource component attached to this GameObject
+		audioSource = GetComponent<AudioSource>();
 	}
 
-    void Start()
-    {
-        waveNumber = 1;
-        zombieNumber = 10;
-        StartCoroutine(WavesTransition());
-    }
-
-    public IEnumerator WavesTransition()
+	// Start is called before the first frame update
+	void Start()
 	{
-        zombieNumber = 5 + 5 * waveNumber;
+		// Initialize the wave number and zombie number
+		waveNumber = 1;
+		zombieNumber = 10;
+
+		// Start the WavesTransition coroutine
+		StartCoroutine(WavesTransition());
+	}
+
+	// This coroutine controls the transition between waves
+	public IEnumerator WavesTransition()
+	{
+		// Calculate the number of zombies to spawn in the next wave
+		zombieNumber = 5 + 5 * waveNumber;
+
+		// Wait for 20 seconds before starting the next wave
 		yield return new WaitForSeconds(20.0f);
+
+		// Start the next wave
 		StartWave();
 	}
 
-    void StartWave()
-    {
-                if (audioSource.isPlaying == false)
-            audioSource.Play();
-        for (int i = 0; i < zombieNumber; i++)
-        {
+	// This method starts a wave of zombie spawns
+	void StartWave()
+	{
+		// If there is no audio playing, start the audio
+		if (audioSource.isPlaying == false)
+			audioSource.Play();
+
+		// Spawn the zombies
+		for (int i = 0; i < zombieNumber; i++)
+		{
+			// Choose a random zombie prefab and spawn point
 			int randomZombie = Random.Range(0, zombiesPrefab.Length);
-            int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
-            Instantiate(zombiesPrefab[randomZombie], spawnPoints[randomSpawnPoint].transform.position, Quaternion.identity);
+			int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+			// Instantiate the zombie at the spawn point
+			Instantiate(zombiesPrefab[randomZombie], spawnPoints[randomSpawnPoint].transform.position, Quaternion.identity);
 		}
+
+		// Increment the wave number for the next wave
 		waveNumber++;
 	}
-
 }
